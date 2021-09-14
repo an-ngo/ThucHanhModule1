4601d93f9abeb75136c294c25aca838e0ad7f12d
an.ngo <phowflowf135@gmail.com>
2021-09-14 10:22:34 +0700

    nop bai

--- cau3.js
+++ cau3.js
@@ -1,7 +1,7 @@
-let str
-let check=['a','o','e','u','i']
+let str;
+let check=['a','o','e','u','i'];
 function kiem_tra_ky_tu_nguyen_am(str) {
-    let count=0
+    let count=0;
     for (const i of check) {
         for(let j=0;j<str.length;j++){
             if(i==str[j]){
@@ -11,7 +11,7 @@ function kiem_tra_ky_tu_nguyen_am(str) {
     }
 
     if(count==0) {return false;}
-    else console.log("so ky tu nguyen am la: "+count)
+    else console.log("so ky tu nguyen am la: "+count);
 }
 
 str=prompt("hay nhap vao chuoi")
